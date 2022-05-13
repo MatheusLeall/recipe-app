509bf03aa1212e05cf66d3aa43d371c40808de61
MatheusLeall <matheuscardoso.pro@gmail.com>
2022-05-13 03:03:39 +0000

    feat: add templates for the views

--- core/settings.py
+++ core/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 import os
+
 from distutils.util import strtobool
 from pathlib import Path
+
+from recipes.apps import RecipesConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    RecipesConfig.name,
 ]
 
 MIDDLEWARE = [
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,15 @@
 from django.http import HttpRequest
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Home view")
+    return render(request, "recipes/home.html")
 
 
 def contact(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Contact view")
+    return render(request, "recipes/contact.html")
 
 
 def about(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("About view")
+    return render(request, "recipes/about.html")
